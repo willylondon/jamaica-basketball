84684606c91749bce931d9b772dda4f89a18a23d
Willy London <willylondon@example.com>
2026-03-11 14:23:52 -0500

    fix: update domain to .online

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://jamaicabasketball.com',
+  siteUrl: process.env.SITE_URL || 'https://jamaicabasketball.online',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   exclude: ['/server-sitemap.xml'], // if applicable
